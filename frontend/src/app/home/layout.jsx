5e17a6d5345140cbc67ff5bb99de668108a03abb
panda <pandaonchain@gmail.com>
2024-12-04 22:23:59 +0100

    fix max-w-screen

--- frontend/src/app/home/layout.jsx
+++ frontend/src/app/home/layout.jsx
@@ -5,12 +5,12 @@ import CreateRep from "./CreateRep";
 
 const HomePage = ({ children }) => {
 	return (
-		<div className="flex h-screen flex-col relative">
-			<div className="w-full flex-none sticky top-0 bg-black">
+		<div className="max-w-screen-sm mx-auto flex h-screen flex-col relative bg-black">
+			<div className="w-full flex-none sticky top-0 ">
 				<HomeHeader />
 			</div>
 			<div className="mb-50">{children}</div>
-			<div className="fixed left-0 right-0 bottom-0 flex mx-2 bg-brand-yellow h-[80px] rounded-t-xl">
+			<div className="fixed left-0 right-0 bottom-0 flex   bg-brand-yellow h-[80px] rounded-t-xl max-w-screen-sm mx-auto">
 				<CreateRep />
 			</div>
 		</div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,34 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        "gray-light": "#1b1d23",
-        "gray-dark": "#131519",
-        "gray-darkest": "#0d0f11",
-        "brand-coal": "#0f0d0e",
-        "brand-charcoal": "#231f20",
-        "brand-gray": "#262522",
-        "brand-yellow": "#fcba28",
-        "brand-pink": "#f38ba3",
-        "brand-green": "#0ba95b",
-        "brand-purple": "#7b5ea7",
-        "brand-beige": "#f9f4da",
-        "brand-blue": "#12b5e5",
-        "brand-orange": "#fc7428",
-        "brand-red": "#ed203d",
-        "brand-white": "#ffffff",
-        gentlePeachLol: "#f99157",
-        magesticPurple: "#9d7dce"
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "#000000",
+				foreground: "#000000",
+				"gray-light": "#1b1d23",
+				"gray-dark": "#131519",
+				"gray-darkest": "#0d0f11",
+				"brand-coal": "#0f0d0e",
+				"brand-charcoal": "#231f20",
+				"brand-gray": "#262522",
+				"brand-yellow": "#fcba28",
+				"brand-pink": "#f38ba3",
+				"brand-green": "#0ba95b",
+				"brand-purple": "#7b5ea7",
+				"brand-beige": "#f9f4da",
+				"brand-blue": "#12b5e5",
+				"brand-orange": "#fc7428",
+				"brand-red": "#ed203d",
+				"brand-white": "#ffffff",
+				gentlePeachLol: "#f99157",
+				magesticPurple: "#9d7dce",
+			},
+		},
+	},
+	plugins: [],
 };
